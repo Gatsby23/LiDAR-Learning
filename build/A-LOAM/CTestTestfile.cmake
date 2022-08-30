01b81ee0cdd4fdd06bdff04272c7d5cfa136ceb0
yepeichu <yepeichu@xa.com>
2022-08-30 19:44:32 +0800

    Feat: remove build

--- build/A-LOAM/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xiaoc/xiaoc/work/XAG/catkin_ws/src/A-LOAM
-# Build directory: /home/xiaoc/xiaoc/work/XAG/catkin_ws/build/A-LOAM
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/LeGO-LOAM/cloud_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xiaoc/xiaoc/work/XAG/catkin_ws/src/LeGO-LOAM/cloud_msgs
-# Build directory: /home/xiaoc/xiaoc/work/XAG/catkin_ws/build/LeGO-LOAM/cloud_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
